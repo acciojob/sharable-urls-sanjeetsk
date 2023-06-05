5f9b90795c53e9eb6df9bd46ab566a5d20fa11bd
sanjeetsk <ssanjeetkumar67@gmail.com>
2023-06-05 23:42:23 +0530

    update

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // your code here
+function handleSubmit(){
+    // e.preventDefault();
+    var name = document.getElementById('name').value;
+    var year = document.getElementById('year').value;
+    let url;
+    if(name !== '' && year !== ''){
+        url = "https://localhost:8080/?name="+name+"&year="+year;
+    }
+    else if(year === ''){
+        url = "https://localhost:8080/?name="+name;
+    }
+    else{
+        url = "https://localhost:8080/?&year="+year;
+    }
+    document.getElementById("url").textContent = url;
+}
